ac170a0be900b3db7bc0924a16a5408eb1050c45
Adam Połomski <adam.polomski@gmail.com>
2016-03-21 23:25:32 +0100

    Initial files.

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+}).listen((process.env.PORT || 5000));
+
+console.log('Server running.');
