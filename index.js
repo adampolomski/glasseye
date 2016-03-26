2c8d37ab1a244c1b12499aa5e957e1740e8cd07c
Adam Połomski <adam.polomski@gmail.com>
2016-03-26 02:08:40 +0100

    Some parsing.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const http = require('http');
+import ChannelRepository from './src/channelRepository';
 
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen((process.env.PORT || 5000));
+var express = require('express');
+var app = express();
+var repo = new ChannelRepository();
 
-console.log('Server running.');
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/channels', function(request, response) {
+  repo.list().then(data => { response.send(data); }).catch(() => { response.send('errori');});
+});
+
+app.get('/channels/:channelId', (request, response) => {
+  response.send('Hello channels!' + request.params.channelId);
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
