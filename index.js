963999d9879e513bd1c5748c5b32f383123ea2da
Adam Połomski <adam.polomski@gmail.com>
2016-10-05 23:30:58 +0200

    Moved stuff around.

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-import ProductRepository from './src/productRepository';
-
-var express = require('express');
-var app = express();
-var repo = new ProductRepository();
+require("babel-core/register");
+const app = require('./src');
 
 app.set('port', (process.env.PORT || 5000));
-
-app.get('/products', function(request, response) {
-  repo.list().then(data => { response.send(data); }).catch(() => { response.send('errori');});
-});
-
-app.get('/products/:productId', (request, response) => {
-  response.send('Hello products!' + request.params.productId);
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
